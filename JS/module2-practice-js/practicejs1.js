f9ba8f6aececf90d86f5d447dacb08884daeed86
inayarajwani <inaya.rajwani@gmail.com>
2019-04-29 09:43:25 -0400

    module 2 answer display

--- JS/module2-practice-js/practicejs1.js
+++ JS/module2-practice-js/practicejs1.js
@@ -7,13 +7,13 @@ function biggerNumber(){
 	console.log(numberTwo); 
 	
 	if(numberOne > numberTwo){
-		document.getElementById("practice1").innerHTML = ("`${numberOne} is the bigger Number`"); 
+		document.getElementById("module2practice").innerHTML = (numberOne + " is the bigger Number"); 
 	} else if (numberTwo > numberOne){
-		document.getElementById("practice1").innerHTML = (`${numberTwo} is the bigger Number`); 
+		document.getElementById("module2practice").innerHTML = (numberTwo + " is the bigger Number"); 
 	} else if(numberTwo==numberTwo){
-		document.getElementById("practice1").innerHTML = ("The numbers are the same"); 
+		document.getElementById("module2practice").innerHTML = ("The numbers are the same"); 
 	} else {	
-		document.getElementById("practice1").innerHTML = ("You didn't enter two numbers"); 
+		document.getElementById("module2practice").innerHTML = ("You didn't enter two numbers"); 
 	}
 }
 
