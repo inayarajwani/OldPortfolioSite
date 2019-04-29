cf77dfd310fd3ca586049b3ed2f64ba18130ac2d
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2019-04-29 09:10:44 -0400

    Update practicejs1.js

--- JS/module2-practice-js/practicejs1.js
+++ JS/module2-practice-js/practicejs1.js
@@ -7,13 +7,16 @@ function biggerNumber(){
 	console.log(numberTwo); 
 	
 	if(numberOne > numberTwo){
-		alert(`${numberOne} is the bigger Number`);
+		document.getElementById("practice1").innerHTML = ("`${numberOne} is the bigger Number`"); 
+		/*alert(`${numberOne} is the bigger Number`);*/
 	} else if (numberTwo > numberOne){
-		alert(`${numberTwo} is the bigger Number`);
+		document.getElementById("practice1").innerHTML = (`${numberTwo} is the bigger Number`); 
+		/*alert(`${numberTwo} is the bigger Number`);*/
 	} else if(numberTwo==numberTwo){
-		alert("The numbers are the same");
+		document.getElementById("practice1").innerHTML = ("The numbers are the same"); 
+		/*alert("The numbers are the same");*/
 	} else {	
-		document.getElementById("practice1").innerHTML = "You didn't enter two numbers"; 
+		document.getElementById("practice1").innerHTML = ("You didn't enter two numbers"); 
 		/*alert("You didn't enter two numbers");*/
 	}
 }
