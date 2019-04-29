40826356abb246adb05fd268b7f77fed46c50196
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2019-04-29 09:12:09 -0400

    Update practicejs1.js

--- JS/module2-practice-js/practicejs1.js
+++ JS/module2-practice-js/practicejs1.js
@@ -8,20 +8,12 @@ function biggerNumber(){
 	
 	if(numberOne > numberTwo){
 		document.getElementById("practice1").innerHTML = ("`${numberOne} is the bigger Number`"); 
-		/*alert(`${numberOne} is the bigger Number`);*/
 	} else if (numberTwo > numberOne){
 		document.getElementById("practice1").innerHTML = (`${numberTwo} is the bigger Number`); 
-		/*alert(`${numberTwo} is the bigger Number`);*/
 	} else if(numberTwo==numberTwo){
 		document.getElementById("practice1").innerHTML = ("The numbers are the same"); 
-		/*alert("The numbers are the same");*/
 	} else {	
 		document.getElementById("practice1").innerHTML = ("You didn't enter two numbers"); 
-		/*alert("You didn't enter two numbers");*/
 	}
 }
 
-/*function biggerNumber() {
-    document.getElementById("practice1").innerHTML = function practice1(); 
-  }
-*/
