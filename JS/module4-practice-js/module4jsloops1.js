7e940c7cd7186c705793aeb08ccd3bb233cc4434
inayarajwani <inaya.rajwani@gmail.com>
2019-05-01 09:45:59 -0400

    module4practice1

---
+++ JS/module4-practice-js/module4jsloops1.js
@@ -0,0 +1,5 @@
+function lengthOfString(){
+	let string = "The length of this string is ";
+	let length = string.length;
+	document.getElementById('module4practice1').innerHTML = (string + length);
+}
