c7ddc4554648cbd19df5fc25d8501738fb614b46
inayarajwani <inaya.rajwani@gmail.com>
2019-05-16 10:41:15 -0400

    encryption final

--- JS/encryption_practice.js
+++ JS/encryption_practice.js
@@ -1,6 +1,19 @@
-function encryptionPigLatinPractice(){
+function encryption(message){
+  let words = message.split(" ");
 
-let secretMessage = prompt("Enter your secret message here");
 
+  let encrypted = "";
+  let listLength = words.length;
+    for (i=0; i<listLength; i++){
+      let firstLetter = words[i][0].toLowerCase();
+      if (firstLetter == "a"||firstLetter == "e"||firstLetter =="i"||firstLetter =="o"||firstLetter =="u"||words[i].length<3){
+      encrypted += words[i] + " ";
+      }else{      
+      let otherLetters = words[i].slice(1);
+      encrypted += otherLetters + words[i][0] + "ay ";
+      }
+    }
+  console.log (encrypted.toLowerCase());
 
 }
+encryption("This is our secret message");
