0f5b9f1ca25499dcbd920970d3d3b0dd1599044b
inayarajwani <inaya.rajwani@gmail.com>
2019-06-05 16:47:27 -0400

    Update practicejs1.js

--- JS/module2-practice-js/practicejs1.js
+++ JS/module2-practice-js/practicejs1.js
@@ -7,9 +7,9 @@ function biggerNumber(){
 	console.log(numberTwo); 
 	
 	if(numberOne > numberTwo){
-		document.getElementById("module2practice1").innerHTML = (numberOne + " is the bigger Number"); 
+		document.getElementById("module2practice1").innerHTML = (numberOne + " is the bigger number"); 
 	} else if (numberTwo > numberOne){
-		document.getElementById("module2practice1").innerHTML = (numberTwo + " is the bigger Number"); 
+		document.getElementById("module2practice1").innerHTML = (numberTwo + " is the bigger number"); 
 	} else if(numberTwo==numberTwo){
 		document.getElementById("module2practice1").innerHTML = ("The numbers are the same"); 
 	} else {	
